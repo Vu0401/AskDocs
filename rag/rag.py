863e0d0e44522978e02c975cb7af48fc00794983
VuzLe <93986576+Vu0401@users.noreply.github.com>
2025-03-10 02:15:08 +0700

    Update rag.py

--- rag/rag.py
+++ rag/rag.py
@@ -6,7 +6,7 @@ load_dotenv()
 os.environ["GEMINI_API_KEY"] = os.getenv('GEMINI_API_KEY')
 
 class RAG:
-    def __init__(self, model="gemini/gemini-2.0-flash", functions=[]) -> None:
+    def __init__(self, model="gemini/gemini-2.0-pro-exp-02-05", functions=[]) -> None:
         self.model = model
         self.functions = functions
         
